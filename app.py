46da9f2739e80d7f2694234ebb540a62b02162b4
folac15 <nchindadavid@gmail.com>
2026-07-31 13:45:31 +0100

    Update app.py

--- app.py
+++ app.py
@@ -68,9 +68,9 @@ def analytics():
 @app.route("/reports.html")
 def reports():
     return send_from_directory(".", "reports.html")
-   
-    
-    @app.route("/automation.html")
+
+
+@app.route("/automation.html")
 def automation():
     return send_from_directory(".", "automation.html")
 
