dc5ac9b18cc815625f98ac0bde8da31ec88eceb1
folac15 <nchindadavid@gmail.com>
2026-08-07 12:47:26 +0100

    Add authentication guard

--- auth_guard.js
+++ auth_guard.js
@@ -1,1 +1,21 @@
+// ===============================
+// NexaFlow AI Authentication Guard
+// ===============================
 
+const token = localStorage.getItem("access_token");
+
+// If user is not logged in
+if (!token) {
+
+    window.location.href = "login.html";
+
+}
+
+// Logout function
+function logout(){
+
+    localStorage.removeItem("access_token");
+
+    window.location.href = "login.html";
+
+                            }
