93570d74e8bfd98177e304f77138a56ba385d9bb
folac15 <nchindadavid@gmail.com>
2026-08-17 07:47:12 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2700,7 +2700,8 @@ def save_integration():
         existing = check.json()
 
         if existing:
-    integration_id = existing[0]["id"]
+    integration_id = existing[0]
+    ["id"]
 
             response = requests.patch(
 
